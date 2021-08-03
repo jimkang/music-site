586338de6c80eb7a10ea4cf5db64135706e11fd9
Jim Kang <jimkang@gmail.com>
2021-08-02 22:48:08 -0400

    Put the important info into the footer. Stop calling a 'footer'.

--- music-site-config.js
+++ music-site-config.js
@@ -8,24 +8,11 @@ module.exports = {
     rootPath,
     maxEntriesPerPage: 25,
     fileAbstractionType: "fs",
-    footerHTML: `<footer id="footer">
-    <div>This site is updated via <a href="https://github.com/jimkang/note-taker">note-taker</a> and <a href="https://github.com/jimkang/note-sender">note-sender</a>. note-taker is an adapter for <a href="https://github.com/jimkang/static-web-archive">static-web-archive</a>.
+    footerHTML: `<div class="important-info">
+    <a href="https://jimkang.bandcamp.com/">Bandcamp</a> |
+    <a href="https://jimkang.com/music/rss/index.rss">RSS feed</a> |
+    <a href="https://jimkang.com">jimkang.com</a>
     </div>
-    <div>
-      <a href="https://jimkang.com/music/rss/index.rss">RSS feed</a>
-    </div>
-    <div>
-      <a href="mailto:jimkang@fastmail.com">jimkang@fastmail.com</a>
-    </div>
-    <div>
-      Want to see more stuff?
-      <ul>
-        <li><a href="https://tinyletter.com/jimkang">A newsletter about projects and plans</a></li>
-        <li><a href="https://smidgeo.com/bots/">Automatically generated art</a></li>
-        <li><a href="https://jimkang.com">jimkang.com</a></li>
-      </ul>
-    </div>
-    </footer>
     `,
     generateRSS: true,
     archiveBaseURL: "https://jimkang.com/music",
